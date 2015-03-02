925b3a21939014fb5bf50a118b276c3ca8d105a8
Yoshiyuka <yoshiyuka@users.noreply.github.com>
2015-03-01 17:01:23 -0800

    Multiple spawn points added to test map. Support for spawning multiple entities at a time added for ruffians.

--- desktop/src-common/hlirgh/entities/ruffian.clj
+++ desktop/src-common/hlirgh/entities/ruffian.clj
@@ -1,5 +1,6 @@
 (ns hlirgh.entities.ruffian
   (:require
+    [play-clj.core :refer :all]
     [play-clj.g2d :refer :all] 
     [hlirgh.entities.utils :refer [cell-from-atlas]]
     [hlirgh.entities.core :refer [create-entity]]))
@@ -17,4 +18,19 @@
 (defn create-ruffian
   [& custom-stats]
   (create-entity ruffian (first custom-stats)))
+
+(defn create-ruffians
+  [screen amount & custom-stats]
+  (let [spawn-layer (tiled-map-layer screen "Utility")
+        spawn-points (filter #(not= (:cell %) nil)
+                             (for [x (range (tiled-map-layer! spawn-layer :get-width))
+                                   y (range (tiled-map-layer! spawn-layer :get-height))]
+                               {:cell (tiled-map-layer! spawn-layer :get-cell x y) :x x :y y}))]
+
+    (for [number (range amount)
+          :let [spawn-point (rand-nth spawn-points)]]
+      (create-ruffian {:x (:x spawn-point) :y (:y spawn-point)}))))
+
+
+
 
